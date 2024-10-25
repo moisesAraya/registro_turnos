730bc6e5efecfe709875b48afd8646ca89254af1
Moises Araya <moises.araya2001@gmail.com>
2024-10-25 18:44:27 -0300

    Agregar imagen de fondo a home y users

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,12 @@
+import '@styles/home.css';
+
 const Home = () => {
   return (
-    <>
-    </>
-  )
-}
+    <div className="main-container">
+      
+      {/* Aquí puedes agregar más contenido, como botones o enlaces */}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
