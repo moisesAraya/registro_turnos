2a3d71f72e781ed28a118444c536825c2e9a8130
tinquinones6 <tinquinones6@MacBook-Air-de-Martin.local>
2024-11-06 17:25:11 -0300

    Selección de area de trabajo y crud completo en backend

---
+++ backend/src/entity/WorkArea.js
@@ -0,0 +1,26 @@
+import { EntitySchema } from "typeorm";
+
+export const WorkArea = new EntitySchema({
+    name: "WorkArea",
+    tableName: "work_areas",
+    columns: {
+        id: {
+            type: "int",
+            primary: true,
+            generated: true,
+        },
+        worker_id: {
+            type: "int",
+            nullable: false,
+        },
+        work_area: {
+            type: "varchar",
+            nullable: false,
+        },
+        shift_date: {
+            type: "date",
+            nullable: false,
+            default: () => "CURRENT_DATE",
+        },
+    },
+});
